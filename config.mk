bffe293c9399f8d7dc518be7700ea2c73beb79de
AndroBuddy <anirudhchimpidi8@gmail.com>
2023-04-16 14:17:35 +0800

    themes: Import Xperia Icon pack
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -192,7 +192,9 @@ PRODUCT_PACKAGES += \
     IconPackOutlineAndroidOverlay \
     IconPackOutlineSystemUIOverlay \
     IconPackAcherusAndroidOverlay \
-    IconPackAcherusSystemUIOverlay
+    IconPackAcherusSystemUIOverlay \
+    IconPackXperiaAndroidOverlay \
+    IconPackXperiaSystemUIOverlay \
 
 # Icon Shapes
 PRODUCT_PACKAGES += \
